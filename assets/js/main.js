b10a56fa06f60e26a5b083a575394d19bef7cec6
Jose <j0s3.a.j1m3n3z@gmail.com>
2015-10-09 23:30:57 -0500

    Congreso v1.0
    Versión 1

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+/*
+	Fractal by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge:		'(max-width: 1680px)',
+		large:		'(max-width: 1280px)',
+		medium:		'(max-width: 980px)',
+		small:		'(max-width: 736px)',
+		xsmall:		'(max-width: 480px)',
+		xxsmall:	'(max-width: 360px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Mobile?
+			if (skel.vars.mobile)
+				$body.addClass('is-mobile');
+			else
+				skel
+					.on('-medium !medium', function() {
+						$body.removeClass('is-mobile');
+					})
+					.on('+medium', function() {
+						$body.addClass('is-mobile');
+					});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly')
+				.scrolly({
+					speed: 1500
+				});
+
+	});
+
+})(jQuery);
+$( "#ponentes" )
+  .mouseenter(function() {
+    $('.flotante h3').text("Ponentes");
+    $(".flotante h3").fadeIn( "slow" );
+  })
+  .mouseleave(function() {
+    $('.flotante h3').text("");
+  });
+$( "#temas" )
+  .mouseenter(function() {
+    $('.flotante h3').text("Temas");
+    $(".flotante h3").fadeIn( "slow" );
+  })
+  .mouseleave(function() {
+    $('.flotante h3').text("");
+  });
+$( "#registro" )
+  .mouseenter(function() {
+    $('.flotante h3').text("Registro en Línea");
+    $(".flotante h3").fadeIn( "slow" );
+  })
+  .mouseleave(function() {
+    $('.flotante h3').text("");
+  });
+
+$( "#proceso" )
+  .mouseenter(function() {
+    $('.flotante h3').text("Proceso de Inscripción");
+    $(".flotante h3").fadeIn( "slow" );
+  })
+  .mouseleave(function() {
+    $('.flotante h3').text("");
+  });
+$( "#cuota" )
+  .mouseenter(function() {
+    $('.flotante h3').text("Cuota de Recuperación");
+    $(".flotante h3").fadeIn( "slow" );
+  })
+  .mouseleave(function() {
+    $('.flotante h3').text("");
+  });
+ $( "#programa" )
+  .mouseenter(function() {
+    $('.flotante h3').text("Programa Provisional");
+    $(".flotante h3").fadeIn( "slow" );
+  })
+  .mouseleave(function() {
+    $('.flotante h3').text("");
+  });
